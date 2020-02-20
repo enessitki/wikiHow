845fd68c23c83d2a2e0722aa6a4d811674832e19
Metetml <60227806+Metetml@users.noreply.github.com>
2020-02-20 10:41:32 +0300

    Update PathEliminationTest.py

--- path_elimination/PathEliminationTest.py
+++ path_elimination/PathEliminationTest.py
@@ -13,20 +13,20 @@ class MyClass:
         pass
 
 
-def calDist(point1,point2):
+def calc_dist(point1,point2):
     x1, y1 = point1
     x2, y2 = point2
     dist = math.sqrt((x2-x1)**2+(y2-y1)**2)
     return dist
 
 
-def calAngle(point1, point2, point3):
+def calc_angle(point1, point2, point3):
     x1, y1 = point1
     x2, y2 = point2
     x3, y3 = point3
     ang = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
-    if ang <0:
-        ang =ang + 360
+    if ang < 0:
+        ang = ang + 360
         if ang > 180:
             ang = 360-ang
         return ang
@@ -36,32 +36,32 @@ def calAngle(point1, point2, point3):
         return ang
 
 
-def maxDist(point1, point2):
-    dist = calDist(point1,point2)
+def max_dist(point1, point2):
+    dist = calc_dist(point1, point2)
     if dist > 20:
         return False
     else:
         return True
 
 
-def minDist(point1, point2):
-    dist = calDist(point1, point2)
+def min_dist(point1, point2):
+    dist = calc_dist(point1, point2)
     if dist < 2.5:
         return False
     else:
         return True
 
 
-def maxAngle(point1, point2, point3):
-    angle = calAngle(point1, point2, point3)
-    if angle < 30:          #ignore angle
+def max_angle(point1, point2, point3):
+    angle = calc_angle(point1, point2, point3)
+    if angle > 160:          #ignore angle
         return True
     else:
         return False
 
 
-def minAngle(point1, point2, point3):
-    angle = calAngle(point1, point2, point3)
+def min_angle(point1, point2, point3):
+    angle = calc_angle(point1, point2, point3)
     if angle < 10:          #little sharp angle
         return True
     else:
@@ -80,109 +80,63 @@ def slope(point1, point2):
     xs2, ys2 = point2
     m = (ys1-ys2) / (xs1-xs2)
     z = (ys2-ys1) / (xs2-xs1)
-    zed=math.atan(z)
-    zedx=math.degrees(zed)
+    zed = math.atan(z)
+    zedx = math.degrees(zed)
     print("angle", zedx)
     print(z)
     print(m)
     return m
 coordinates = [(x[0]*100, x[1]*100) for x in numpy.random.rand(45, 2)]
 print(coordinates)
-codes= [
-    Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO,
-    Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO
-]
-l1=len(coordinates)
-#orginal path
-path=Path(coordinates, codes)
+codes = []
+codes.append(Path.MOVETO)
+codes.append(Path.LINETO)
+l1 = len(coordinates)
+for i in range(0, l1-2):
+    codes.append(Path.LINETO)
 
-fig, ax=plt.subplots()
-patch= patches.PathPatch(path, facecolor='none', lw=2)
+#orginal path
+path = Path(coordinates, codes)
+
+fig, ax = plt.subplots()
+patch = patches.PathPatch(path, facecolor='none', lw=2)
 ax.add_patch(patch)
 ax.set_xlim(0, 100)
 ax.set_ylim(0, 100)
 
-#3-dot elimination
-x1=int((l1)/2)-1
-coor2=[]
-coor2.append(coordinates[0])
-codes2=[]
-codes2.append((codes[0]))
 
-for i in range(0, x1):
-    coor2.append(coordinates[2*i+1])
-    codes2.append(codes[2*i+1])
-coor2.append(coordinates[l1-1])
-codes2.append((codes[l1-1]))
-path2 = Path(coor2, codes2)
-
-fig2, ax2 = plt.subplots()
-patch2= patches.PathPatch(path2, facecolor='none', lw=2)
-ax2.add_patch(patch)
-ax2.set_xlim(0, 100)
-ax2.set_ylim(0, 100)
-
-#----------------------------------------------
-#Burası Karıştı.Hepsini burda toplamaya çalıştım ancak max distance kontrolü 1den fazla yerde yapılması gerektiğini farkettim.
-test=[]
+test = []
 test.append(coordinates[0])
-testCode=[]
+test.append(coordinates[1])
+testCode = []
 testCode.append(Path.MOVETO)
-for i in range(2,l1-1):
-    tfmin=minDist(coordinates[i-1],coordinates[i])
-    tfmax=maxDist(coordinates[i-1], coordinates[i])
-    if tfmax == True:
-        if tfmin== True:
-            tfMinAngle = minAngle(coordinates[i-2], coordinates[i-1], coordinates[i])
-            tfMaxAngle = maxAngle(coordinates[i-2], coordinates[i-1], coordinates[i])
-            if tfMinAngle < True and tfMaxAngle > True:
-                test.appent(coordinates[i])
+testCode.append(Path.LINETO)
+for i in range(2, l1-1):
+    tf_max_dist = max_dist(coordinates[i - 1], coordinates[i])
+    tf_max_dist2 = max_dist(coordinates[i - 2], coordinates[i])
+    tf_min_dist = min_dist(coordinates[i - 1], coordinates[i])
+    tf_max_ang = max_angle(coordinates[i - 2], coordinates[i - 1], coordinates[i])
+    tf_min_ang = min_angle(coordinates[i - 2], coordinates[i - 1], coordinates[i])
+    if tf_max_dist == True:
+        if tf_min_dist == True:
+            if tf_max_dist2 == True:
+                if tf_max_ang == True or tf_min_ang == True:
+                    test.append(coordinates[i])
+                    testCode.append(Path.LINETO)
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
     else:
         test.append(coordinates[i])
-ltest=len(test)
-for i in range(1,ltest):
-    testCode.append(Path.LINETO)
-
-for i in range(1, l1):
-    dist = calDist(coordinates[i], coordinates[k])
-    if dist > 2.5:
-        test.append(coordinates[i])
         testCode.append(Path.LINETO)
-        k = i
 path3 = Path(test, testCode)
 fig3, ax3 = plt.subplots()
 patch3 = patches.PathPatch(path3, facecolor='none', lw=2)
 ax3.add_patch(patch3)
 ax3.set_xlim(0, 100)
 ax3.set_ylim(0, 100)
-#-------------------------------------------------------
-
-#both check minAngle and minDistance
-testAng=[]
-testAng.append(coordinates[0])
-testAng.append(coordinates[1])
-testCodeAng=[]
-testCodeAng.append(Path.MOVETO)
-testCodeAng.append(Path.LINETO)
-
-for i in range(1, l1):
-    if i+2 < l1:
-        if minDist(coordinates[i-1], coordinates[i]) == True:
-            angThree = calAngle(coordinates[i-1], coordinates[i], coordinates[i+1])
-            if angThree > 10:
-                if maxDist(coordinates[i-1], coordinates[i]) == True:
-                    testAng.append(coordinates[i+2])
-                    testCodeAng.append(Path.LINETO)
-path4=Path(testAng, testCodeAng)
-fig4, ax4 = plt.subplots()
-patch4 = patches.PathPatch(path4, facecolor='none', lw=2)
-ax4.add_patch(patch4)
-ax4.set_xlim(0, 100)
-ax4.set_ylim(0, 100)
+#Use this for graphs
+#plt.show()
