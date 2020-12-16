0955088fcb6318878ff697df5de665b98492cea9
Metetml <60227806+Metetml@users.noreply.github.com>
2020-12-16 15:58:55 +0300

    mapping range-angle update

--- stereo_cam_depth_test/realsense-point-cloud-registration-3.py
+++ stereo_cam_depth_test/realsense-point-cloud-registration-3.py
@@ -7,6 +7,8 @@ import time
 from classes.D435 import D435
 from classes.T265 import T265
 from scipy.spatial.transform import Rotation as R
+import pyrealsense2 as rs
+import open3d as o3d
 # notes
 # https://github.com/yoshimasa1700/mono_vo_python
 # http://campar.in.tum.de/files/saleh/final_pres_reconstruct.pdf
@@ -15,6 +17,7 @@ from scipy.spatial.transform import Rotation as R
 # import pyqtgraph.examples
 # pyqtgraph.examples.run()
 
+pc = rs.pointcloud()
 
 class Window(gl.GLViewWidget):
     def __init__(self, parent=None):
@@ -52,60 +55,108 @@ class Window(gl.GLViewWidget):
         self.inverseSensitivity = 100
         self.frameWidth = 640
         self.frameHeight = 480
-        self.maxDistance = 1
+        self.maxDistance = 2
+        self.pitch = 0
+        self.yaw = 0
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.start = time.time()
+        self.time = time.time()
 
         self.show()
 
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(10)
+        self.timer.setInterval(100)
         self.timer.timeout.connect(self.run)
         self.timer.start()
 
     def run(self):
         # self.vertsMem = np.empty((0, 3), float)
         # self.colorMem = np.empty((0, 4), float)
+        distance = 0
+        angle = 0
         t0 = time.time()
 
         verts, texcoords, color_image = self.d435.update_frames()
         translation, rotation = self.t265.update_frames()
+        x, y, z = translation
+        roll, pitch, yaw = rotation
         translation = np.array(translation)
         r = R.from_euler("xyz", rotation, degrees=True)
-        if verts is not None:
-            k = -1
-            for vert, tex in zip(verts, texcoords):
-                k += 1
-                if vert[2] < self.maxDistance:
-                    vert = r.apply(vert)
-                    vert += translation
-                    vert[1], vert[2] = vert[2], -vert[1]
-                    vert[0] = int(vert[0]*self.inverseSensitivity)
-                    vert[1] = int(vert[1]*self.inverseSensitivity)
-                    vert[2] = int(vert[2]*self.inverseSensitivity)
+        if abs(self.x - x) > 0.5 or abs(self.y - y) > 0.5 or abs(self.z - z) > 0.5:
+            distance = 1
+        if (abs(self.pitch - pitch) > 5) or (abs(self.yaw - yaw > 5)):
+            angle = 1
+        print(distance, angle)
+        if distance == 1 or angle == 1:
+            if verts is not None:
+                k = -1
+                for vert, tex in zip(verts, texcoords):
+                    k += 1
+                    if vert[2] < self.maxDistance:
+                        vert = r.apply(vert)
+                        vert += translation
+                        vert[1], vert[2] = vert[2], -vert[1]
+                        vert[0] = int(vert[0]*self.inverseSensitivity)
+                        vert[1] = int(vert[1]*self.inverseSensitivity)
+                        vert[2] = int(vert[2]*self.inverseSensitivity)
 
-                    x = int(tex[0] * self.frameWidth + 0.5)
-                    y = int(tex[1] * self.frameHeight + 0.5)
+                        x = int(tex[0] * self.frameWidth + 0.5)
+                        y = int(tex[1] * self.frameHeight + 0.5)
 
-                    if 0 <= x < self.frameWidth and 0 <= y < self.frameHeight:
-                        rgb = np.array([x for x in np.flip(color_image[y, x, :]) / 255] + [1])
-                        # if rgb[0] > 0.8 and rgb[1] > 0.8 and rgb[2] > 0.8:
-                        #     rgb = np.array([0, 0, 0, 0])
-                    else:
-                        rgb = np.array([0, 0, 0, 0])
+                        if 0 <= x < self.frameWidth and 0 <= y < self.frameHeight:
+                            rgb = np.array([x for x in np.flip(color_image[y, x, :]) / 255] + [1])
+                            # if rgb[0] > 0.8 and rgb[1] > 0.8 and rgb[2] > 0.8:
+                            #     rgb = np.array([0, 0, 0, 0])
+                        else:
+                            rgb = np.array([0, 0, 0, 0])
 
-                    key = str(vert)
-                    if key not in self.filterHash:
-                        self.vertsMem = np.append(self.vertsMem, np.array([vert]), axis=0)
-                        self.colorMem = np.append(self.colorMem, np.array([rgb]), axis=0)
-                        self.filterHash[key] = len(self.vertsMem) - 1
-                    else:
-                        self.colorMem[self.filterHash[key]] = (self.colorMem[self.filterHash[key]] + rgb*5)/6
+                        key = str(vert)
+                        if key not in self.filterHash:
+                            self.vertsMem = np.append(self.vertsMem, np.array([vert]), axis=0)
+                            self.colorMem = np.append(self.colorMem, np.array([rgb]), axis=0)
+                            self.filterHash[key] = len(self.vertsMem) - 1
+                        else:
+                            self.colorMem[self.filterHash[key]] = (self.colorMem[self.filterHash[key]] + rgb*5)/6
+                print("x: ", self.x - x)
+                print("y: ", self.y - y)
+                print("z: ", self.z - z)
+                print("yaw: ", self.yaw)
+                # print("roll: ", self.roll)
+                print("pitch", self.pitch)
+                self.pitch = pitch
+                self.yaw = yaw
+                self.x = x
+                self.y = y
+                self.z = z
 
             if len(self.vertsMem) > 1:
                 self.sp.setData(pos=self.vertsMem*(10/self.inverseSensitivity), color=self.colorMem, size=10/self.inverseSensitivity)
 
-        cv2.imshow("colored", color_image)
+        print("x::::-", x)
+        # cv2.imshow("colored", color_image)
         # print(len(self.filterHash))
+        # if time.time() - self.start > 20 and time.time() - self.start < 22:
+        #     print("out")
+        #     pcd = o3d.geometry.PointCloud()
+        #     pcd.points = o3d.utility.Vector3dVector(texcoords)
+        #     o3d.io.write_point_cloud("./new.pcd", pcd)
+        #     o3d.visualization.draw_geometries([pcd])
+        # else:
+        #     print("error")
+
+        key = cv2.waitKey(1)
+        if key == ord("s"):
+            self.timer.disconnect()
+        # print(self.time - time.time())
+        # if (time.time() - self.time) > 60:
+        #     self.timer.disconnect()
+        if time.time() - t0 < 1:
+            time.sleep(1)
         print(time.time() - t0)
+        # print("translation : ", translation)
+        # print("rotatition : ", rotation)
 
 
 app = QtWidgets.QApplication(sys.argv)
