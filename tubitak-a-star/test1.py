fc4c4d01a9caf3f2b665d57ceff2b9eefd3f1263
Recep <sungurtasrecep@gmail.com>
2020-12-17 00:48:03 -0800

    A star editted

--- tubitak-a-star/test1.py
+++ tubitak-a-star/test1.py
@@ -28,9 +28,7 @@ class Window(QWidget):
         self.colorNumber = 0
         self.colorCodes = ["#FF0000", "#FFC900", "#8FFF00", "#00FF3A", "#00FFC5", "#00B2FF", "#0049FF", "#8F00FF",
                            "#2A7C7A", "#606120", "#3E6632"]
-        self.timeStart = None
-        self.timePathFin = None
-        self.timeDrawFin = None
+        self.index = 0
 
         self.startMarker = None
         self.goalMarker = None
@@ -41,9 +39,8 @@ class Window(QWidget):
         self.open_set, self.closed_set = dict(), dict()
         self.obstacleList = []
 
-        self.stepSize = 1
+        self.stepSize = 10
         self.obstacleRadius = 10
-        self.motion = self.get_motion_model(self.stepSize)
 
         self.geodesic = geodesic()
 
@@ -83,7 +80,7 @@ class Window(QWidget):
         btn_obstacle = QPushButton("Obstacle")
         btn_start.clicked.connect(self.start_btn_clicked)
         btn_goal.clicked.connect(self.goal_btn_clicked)
-        btn_step.clicked.connect(self.run)
+        btn_step.clicked.connect(self.planning)
         btn_obstacle.clicked.connect(self.obstacle_btn_clicked)
 
         layout.addLayout(info_layout)
@@ -116,23 +113,10 @@ class Window(QWidget):
             self.map.removeLayer(self.startMarker)
 
     def obstacle_btn_clicked(self):
-        self.buttonMode = "OBSTACLE"
+        # self.buttonMode = "OBSTACLE"
         self.open_set.clear()
-        self.open_set[self.calc_index(self.start_node)] = self.start_node
-        self.goal_node.grid = self.calc_goal_grid()
-        print("Start Node x :", self.start_node.x)
-        print("Start Node y :", self.start_node.y)
-        print("Start Node cost :", self.start_node.y)
-        print("Start Node parent_index :", self.start_node.parent_index)
-        print("Start Node grid :", self.start_node.grid)
-
-        print("Goal Node x :", self.goal_node.x)
-        print("Goal Node y :", self.goal_node.y)
-        print("Goal Node cost :", self.goal_node.y)
-        print("Goal Node parent_index :", self.goal_node.parent_index)
-        print("Goal Node grid :", self.goal_node.grid)
-
-        # x, y, cost, parent_index, grid
+        start_id = self.calc_index(self.start_node)
+        self.open_set[start_id] = self.start_node
 
     def goal_btn_clicked(self):
         self.buttonMode = "GOAL"
@@ -150,8 +134,9 @@ class Window(QWidget):
             self.startMarker = L.circleMarker(startPoint, {"color": "#3FF00F", "radius": 5})
             self.startMarker.bindPopup('START')
             self.map.addLayer(self.startMarker)
-            self.start_node = self.Node(x=startPoint[0], y=startPoint[1], cost=0.0, parent_index=-1, grid=(0, 0))
-            ##@@print(startPoint)
+            self.start_node = self.Node(x=startPoint[0], y=startPoint[1], cost=0.0, parent_index=-1,
+                                        poly=self.calc_polygon(startPoint[0], startPoint[1], self.stepSize))
+            print(startPoint)
 
         elif self.buttonMode == "GOAL":
             goalPoint = [round(float(point[0]), 6), round(float(point[1]), 6)]
@@ -160,8 +145,8 @@ class Window(QWidget):
             self.goalMarker = L.circleMarker(goalPoint, {"color": '#F0370F', "radius": 5})
             self.goalMarker.bindPopup('GOAL')
             self.map.addLayer(self.goalMarker)
-            self.goal_node = self.Node(x=goalPoint[0], y=goalPoint[1], cost=None, parent_index=None, grid=None)
-
+            self.goal_node = self.Node(x=goalPoint[0], y=goalPoint[1], cost=0.0, parent_index=-2,
+                                       poly=self.calc_polygon(goalPoint[0], goalPoint[1], self.stepSize))
 
         elif self.buttonMode == "OBSTACLE":
             obstaclePoint = [round(float(point[0]), 6), round(float(point[1]), 6)]
@@ -171,79 +156,55 @@ class Window(QWidget):
             self.obstacleList.append(self.calc_polygon(obstaclePoint[0], obstaclePoint[1], self.obstacleRadius))
 
     class Node:
-        def __init__(self, x, y, cost, parent_index, grid):
+        def __init__(self, x, y, cost, parent_index, poly):
             self.x = x  # index of grid
             self.y = y  # index of grid
             self.cost = cost
             self.parent_index = parent_index
-            self.grid = grid
+            self.poly = poly
 
     @staticmethod
     def calc_index(n1):
-        return n1.grid
+        # self.index += 1
+        # return self.index
+        return n1.x, n1.y
 
-    @staticmethod
-    def get_motion_model(step):
-        # dx, dy, cost, angle
-        motion = [[0, 1, step, 0],
-                  [1, 1, step * 1.4, 45],
-                  [1, 0, step, 90],
-                  [1, -1, step * 1.4, 135],
-                  [0, -1, step, 180],
-                  [-1, -1, step * 1.4, 225],
-                  [-1, 0, step, 270],
-                  [-1, 1, step * 1.4, 315]]
+    def is_goal_closer_than_step(self, node):
+        # result = None
+        # min_dist_id = min(self.open_set,
+        #                   key=lambda o: self.geodesic.measure((self.open_set[o].x, self.open_set[o].y),
+        #                                                       (self.goal_node.x, self.goal_node.y)))
+        # if self.stepSize >= (self.geodesic.measure((self.open_set[min_dist_id].x, self.open_set[min_dist_id].y),
+        #                                            (self.goal_node.x, self.goal_node.y)) * 1000):
+        #     result = min_dist_id
+        dist = self.geodesic.measure((node.x, node.y), (self.goal_node.x, self.goal_node.y))*1000
+        result = False
+        if dist <= self.stepSize:
+            result = True
+        return result
 
-        return motion
-
-    def calc_goal_grid(self):
-        xMultiplier = 1
-        yMultiplier = 1
-        if self.goal_node.x < self.start_node.x:
-            yMultiplier = -1
-        elif self.goal_node.x == self.start_node.x:
-            yMultiplier = 0
-        if self.goal_node.y < self.start_node.y:
-            xMultiplier = -1
-        elif self.goal_node.y == self.start_node.y:
-            xMultiplier = 0
-        startNode = (self.start_node.x, self.start_node.y)
-        forYpoint = (self.goal_node.x, self.start_node.y)
-        forXpoint = (self.start_node.x, self.goal_node.y)
-        xDist = self.geodesic.measure(startNode, forXpoint) * 1000
-        yDist = self.geodesic.measure(startNode, forYpoint) * 1000
-        gridX = (int(xDist / self.stepSize)) * xMultiplier
-        gridY = (int(yDist / self.stepSize)) * yMultiplier
-        return gridX, gridY
-
-    # def is_goal_closer_than_step(self):
-    #     result = None
-    #     min_dist_id = min(self.open_set,
-    #                key=lambda o: self.geodesic.measure((self.open_set[o].x,self.open_set[o].y), (self.goal_node.x, self.goal_node.y)))
-    #     if self.stepSize >= (self.geodesic.measure((self.open_set[min_dist_id].x,self.open_set[min_dist_id].y),
-    #                                               (self.goal_node.x, self.goal_node.y))*1000):
-    #         result = min_dist_id
-    #     return result
-
-    def calc_neighbor(self, current, i):
-        newGrid = (current.grid[0] + self.motion[i][0], current.grid[1] + self.motion[i][1])
-        if newGrid in self.open_set:
-            return None  # Open set içinde varsa None döner
+    def calc_neighbor(self, n1, i):
+        p1 = geopy.Point(n1.x, n1.y)
+        if i % 2 == 0:
+            step = self.stepSize
         else:
-            p1 = geopy.Point(current.x, current.y)
-            d = geopy.distance.geodesic(kilometers=self.motion[i][2] / 1000)
-            coord = d.destination(point=p1, bearing=self.motion[i][3]).format_decimal()
-            coord = coord.split(",")
-            x = round(float(coord[0]), 6)
-            y = round(float(coord[1]), 6)
-            cost = current.cost + self.motion[i][2]
-            return [x, y, cost, newGrid]
+            step = self.stepSize * 1.4
+
+        d = geopy.distance.geodesic(kilometers=step / 1000)
+        coord = d.destination(point=p1, bearing=45 * i).format_decimal()
+        coord = coord.split(",")
+        x = round(float(coord[0]), 6)
+        y = round(float(coord[1]), 6)
+        cost = n1.cost + step
+        result = [x, y, cost]
+        print("Neighbor", i, ":", result)
+        return [x, y, cost]
 
     def is_in_obstacle(self, n1):
         result = False
         p1 = Point(n1.x, n1.y)
-        for node in self.obstacleList:
-            result = node.contains(p1)
+        for poly in self.obstacleList:
+            result = poly.contains(p1)
             if result:
                 return result
         return result
@@ -258,19 +219,19 @@ class Window(QWidget):
         if not result:
             self.obstacleList.append(self.calc_polygon(x, y, self.obstacleRadius))
 
-    # def is_in_openSet(self, n1, n_id):
-    #     result = False
-    #     if n_id in dict.keys(self.open_set):
-    #         result = True
-    #         return result
-    #     else:
-    #         p1 = Point(n1.x, n1.y)
-    #         for node in self.open_set.values():
-    #             poly = node.poly
-    #             result = poly.contains(p1)
-    #             if result:
-    #                 return result
-    #     return result
+    def is_in_openSet(self, n1, n_id):
+        result = False
+        if n_id in dict.keys(self.open_set):
+            result = True
+            return result
+        else:
+            p1 = Point(n1.x, n1.y)
+            for node in self.open_set.values():
+                poly = node.poly
+                result = poly.contains(p1)
+                if result:
+                    return result
+        return result
 
     @staticmethod
     def calc_polygon(x, y, stepSize):
@@ -302,13 +263,9 @@ class Window(QWidget):
             rx.append(n.x)
             ry.append(n.y)
             parent_index = n.parent_index
-        self.timePathFin = time.time()
 
         for i in range(len(rx) - 1):
             self.draw_line(rx[i], ry[i], rx[i + 1], ry[i + 1])
-        self.timeDrawFin = time.time()
-        print("Path Find Time : ", (self.timePathFin-self.timeStart))
-        print("Draw Time", (self.timeDrawFin-self.timeStart))
 
     def draw_line(self, x1, y1, x2, y2):
         pathMarkers = L.polyline([[x1, y1], [x2, y2]], {
@@ -321,50 +278,41 @@ class Window(QWidget):
         pathMarkers.addTo(self.map)
 
     def planning(self):
-
-        time1 = time.time()
-
         if len(self.open_set) == 0:
             print("Open set is empty..")
             return 0
 
-        timeMin1 = time.time()
         c_id = min(self.open_set,
                    key=lambda o: self.open_set[o].cost + self.calc_heuristic(self.goal_node, self.open_set[o]))
-        timeMin2 = time.time()
-        print("Min bulma zamanı : ", timeMin2-timeMin1)
-        ##@@print("seçilen ID : ", c_id)
+        print("seçilen ID : ", c_id)
         current = self.open_set[c_id]
-        ##@@print("current.grid : ", current.grid)
-        # #####################################################
-        # self.current_marker = L.circleMarker([current.x, current.y], {"color": "#000000", "radius": 1})
-        # self.current_marker.bindPopup(c_id)
-        # self.map.addLayer(self.current_marker)
-        # #####################################################
+        #####################################################
+        self.current_marker = L.circleMarker([current.x, current.y], {"color": "#000000", "radius": 1})
+        self.current_marker.bindPopup('current')
+        self.map.addLayer(self.current_marker)
+        #####################################################
 
-        if self.goal_node.grid in self.open_set:
-            print("Find goal")
-            self.goal_node.parent_index = self.open_set[self.goal_node.grid].parent_index
-            self.goal_node.cost = current.cost
+        # newGoalNodeParentIndex = self.is_goal_closer_than_step(current)
+        #
+        # if newGoalNodeParentIndex is not None:
+        #     print("Find goal")
+        #     self.goal_node.parent_index = newGoalNodeParentIndex
+        #     self.calc_final_path()
+        #     return 1
+
+        if self.is_goal_closer_than_step(current):
+            self.goal_node.parent_index = c_id
             self.calc_final_path()
-            time2 = time.time()
-            print("time1-time :", (time2- time1))
-
-            a = self.start_node.x, self.start_node.y
-            b = self.goal_node.x, self.goal_node.y
-            print("Mesafe : ", self.geodesic.measure(a, b) * 1000)
-            return 1
 
         del self.open_set[c_id]
         self.closed_set[c_id] = current
 
         color = self.colorPicker()
 
-        for i in range(len(self.motion)):  # 8 adet komşu
+        for i in range(8):  # 8 adet komşu tek sayılar karenin köşeleri çift sayılar karenin kenarlarının ortası
             neighbor = self.calc_neighbor(current, i)
-            if neighbor is None:
-                continue
-            node = self.Node(x=neighbor[0], y=neighbor[1], cost=neighbor[2], parent_index=c_id, grid=neighbor[3])
+            node = self.Node(x=neighbor[0], y=neighbor[1], cost=neighbor[2], parent_index=c_id,
+                             poly=self.calc_polygon(neighbor[0], neighbor[1], self.stepSize))
             n_id = self.calc_index(node)
 
             # If the node is not safe, do nothing
@@ -374,25 +322,21 @@ class Window(QWidget):
             if n_id in self.closed_set:
                 continue
 
+            # if not self.is_in_openSet(node, n_id):
             if n_id not in self.open_set:
                 self.open_set[n_id] = node
-                # ######################################################
-                # self.neighbor_marker = L.circleMarker([node.x, node.y], {"color": color, "radius": 1})
-                # self.neighbor_marker.bindPopup(n_id)
-                # self.map.addLayer(self.neighbor_marker)
-                # ######################################################
-            else:
-                if self.open_set[n_id].cost > node.cost:
-                    # This path is the best until now. record it
-                    self.open_set[n_id] = node
+                ######################################################
+                self.neighbor_marker = L.circleMarker([node.x, node.y], {"color": color, "radius": 1})
+                self.neighbor_marker.bindPopup('neighbor')
+                self.map.addLayer(self.neighbor_marker)
+                ######################################################
 
-    def run(self):
-        self.timeStart = time.time()
-        # self.planning()
-        result = None
-        while result == None:
-            result = self.planning()
 
+
+            # else:
+            #     if open_set[n_id].cost > node.cost:
+            #         # This path is the best until now. record it
+            #         open_set[n_id] = node
 
 
 app = QApplication(sys.argv)
